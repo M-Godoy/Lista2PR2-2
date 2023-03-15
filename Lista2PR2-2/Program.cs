e9dc8835807319da253e54e935523de3cdc4579b
M-Godoy <godoy.s@aluno.ifso.edu.br>
2023-03-15 12:16:51 -0300

    até atividade 5 completa

--- Lista2PR2-2/Program.cs
+++ Lista2PR2-2/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,129 @@ namespace Lista2PR2_2
     {
         static void Main(string[] args)
         {
+            //atividade 1
+            //float l = 0.35f, nl, d= 0.28f, vt;
+
+            //Console.WriteLine("Escreva quantas laranjas vc uqer comprar: ");
+            //nl = float.Parse(Console.ReadLine());
+
+
+            //if (nl >= 12)
+            //{
+            //    vt = d * nl;
+            //}
+            //else
+            //{
+            //    vt= l * nl;
+            //}
+
+            //Console.WriteLine("O valor total de sua compra de laranjas é igual a:" + vt );
+            //Console.ReadKey();
+
+
+            ////atividade 2
+            //int n, a=2023;
+
+            //Console.WriteLine("digite qual o ano do seu nacimento: ");
+            //n = int.Parse(Console.ReadLine());
+
+            //if((a - n) >= 16)
+            //{
+            //    Console.WriteLine("Você tem direito ao voto ");
+            //    Console.ReadKey();
+
+            //}
+            //else
+            //{
+            //    Console.WriteLine("você ainda não tem direito ao voto");
+            //    Console.ReadKey();
+            //}
+
+            ////atividade 3
+            //float l1, l2, l3;
+
+            //Console.WriteLine("Digite o valor do primeiro lado: ");
+            //l1 = float.Parse(Console.ReadLine());
+            //Console.WriteLine("Digite o valor do segundo lado: ");
+            //l2 = float.Parse(Console.ReadLine());
+            //Console.WriteLine("Digite o valor do terceiro lado: ");
+            //l3 = float.Parse(Console.ReadLine());
+
+            //if (l1 == l2 && l1 == l3)
+            //{
+            //    Console.WriteLine("Seu triângulo é equilátero");
+            //    Console.ReadKey();
+
+
+            //}
+            //else if (l1 == l2 || l1 == l3)
+            //{
+            //    Console.WriteLine("Seu triângulo é isóceles");
+            //    Console.ReadKey();
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Seu triângulo é escaleno");
+            //    Console.ReadKey();
+            //}
+
+            //atividade 4
+
+            //int a, b, c, x1, x2, delta;
+
+            //Console.WriteLine("Digite o valor refêrente ao valor A: ");
+            //a = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Digite o valor refêrente ao valor B: ");
+            //b = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Digite o valor refêrente ao valor C: ");
+            //c = int.Parse(Console.ReadLine());
+
+            //delta = (b * b) - 4 * a * c;
+
+            //if (delta <= 0)
+            //{
+            //    Console.WriteLine("Não há resultado");
+            //    Console.ReadKey();
+            //}
+            //else
+            //{
+            //    x1 = ((b * -1) + Math.sqrt (delta) ) / (2 * a);
+            //    x2 = ((b * -1) - Math.sqrt (delta) ) / (2 * a);
+
+            //    Console.WriteLine("O resultado de x1 é igual a: " + x1);
+            //    Console.WriteLine("O resultado de x2 é igual a: " + x2);
+            //    Console.ReadKey();
+
+            //}
+            //atividade 5
+            //int v;
+
+            //Console.WriteLine("Digite um valor inteiro; ");
+            //v = int.Parse(Console.ReadLine());
+
+            //if (v % 3 == 0 && v % 5 == 0)
+            //{
+            //    Console.WriteLine("Seu valor é divisível por ambos, 3 e 5.");
+            //    Console.ReadKey();
+
+            //}
+            //else if (v % 3 == 0)
+            //{
+            //    Console.WriteLine("Seu valor é divisívle por 3, apenas.");
+            //    Console.ReadKey();
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Seu valor é divisível por 5, apenas.");
+            //    Console.ReadKey();
+            //}
+
+            //atividade 6
+            float a1, a2, a3;
+
+            Console.WriteLine();
+
+
         }
     }
 }
